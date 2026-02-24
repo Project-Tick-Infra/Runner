e4d757e813b18954f02a0f8a3339b49371f55ec5
Project Tick Bridge <bridge@projecttick.org>
2026-02-24 18:05:26 +0000

    base: project-tick/core/refraction (main)

---
+++ src/api/dadjoke.rs
@@ -0,0 +1,17 @@
+use super::HttpClient;
+
+use eyre::Result;
+
+const DADJOKE: &str = "https://icanhazdadjoke.com";
+
+pub async fn get_joke(http: &HttpClient) -> Result<String> {
+	let joke = http
+		.get(DADJOKE)
+		.header("Accept", "text/plain")
+		.send()
+		.await?
+		.text()
+		.await?;
+
+	Ok(joke)
+}
